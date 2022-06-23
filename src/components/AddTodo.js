06c38bea093c3379749074ed4ecb906589bb91d8
dymafr <erwan1309@hotmail.com>
2022-06-23 13:08:38 +0200

    1 file modified

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function AddTodo({ addTodo }) {
+  const [value, setValue] = useState('');
+
+  function handleChange(e) {
+    const inputValue = e.target.value;
+    setValue(inputValue);
+  }
+
+  function handleClick() {
+    if (value.length) {
+      addTodo(value);
+      setValue('');
+    }
+  }
+
   return (
     <div className="d-flex justify-content-center align-items-center mb-20">
       <input
         type="text"
+        onChange={handleChange}
         className="mr-15 flex-fill"
         placeholder="Ajouter une tâche"
       />
-      <button className="btn btn-prinmary">Ajouter</button>
+      <button onClick={handleClick} className="btn btn-prinmary">
+        Ajouter
+      </button>
     </div>
   );
 }
